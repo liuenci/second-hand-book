5d00a327db0337578998502225374f018b756285
liuenci <154910381@qq.com>
2019-04-20 20:12:53 +0800

    update sell book

--- vbook-backend/src/main/java/com/gxy/vbook/controller/user/BookController.java
+++ vbook-backend/src/main/java/com/gxy/vbook/controller/user/BookController.java
@@ -1,10 +1,12 @@
 package com.gxy.vbook.controller.user;
 
+import com.gxy.vbook.common.Const;
 import com.gxy.vbook.common.PageResponse;
 import com.gxy.vbook.common.ServerResponse;
 import com.gxy.vbook.pojo.User;
 import com.gxy.vbook.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,25 +20,16 @@ import java.util.List;
 @RequestMapping("book")
 public class BookController {
     @Autowired
+    private RedisTemplate<String,String> redisTemplate;
+    @Autowired
     private BookService bookService;
     @RequestMapping("add")
-    public ServerResponse add(@RequestParam("userId") Integer id, @RequestParam("name") String name, @RequestParam("price") Double price) {
-        return bookService.save(id,name,price);
+    public ServerResponse add(@RequestParam("name") String name, @RequestParam("price") Double price) {
+        Integer userId = Integer.parseInt(redisTemplate.opsForValue().get(Const.CURRENT_USER));
+        return bookService.save(userId,name,price);
     }
     @GetMapping("list")
     public PageResponse findList(@RequestParam(value = "name",required = false,defaultValue = "") String name) {
         return bookService.findList(name);
-    }
-    @RequestMapping("l")
-    public HashMap<String,Object> list(){
-        HashMap<String,Object> map = new HashMap<>();
-        User user = new User();
-        user.setId(1);
-        user.setName("name");
-        map.put("total",1);
-        List<User> list = new ArrayList<>();
-        list.add(user);
-        map.put("rows",list);
-        return map;
     }
 }
