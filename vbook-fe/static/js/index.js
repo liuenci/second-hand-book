5ab74eba3772040c2a9c640e46651c9f7f7059a0
liuenci <154910381@qq.com>
2019-05-01 22:36:00 +0800

    update many detail

--- vbook-fe/static/js/index.js
+++ vbook-fe/static/js/index.js
@@ -58,5 +58,31 @@ function getDataList() {
     })
 }
 $(function () {
+    // 判断是否在线
+    $.ajax({
+        url: 'http://localhost:8080/user/online',
+        async: false,
+        success: function (result) {
+            if (result.status == 0) {
+                $('#btn-user-login').remove();
+                $('#btn-user-register').remove();
+                $('#name').text(result.data.name)
+            } else {
+                $('#btn-user-center').remove();
+            }
+
+            $('#logout').click(function () {
+                $.ajax({
+                    url: 'http://localhost:8080/user/logout',
+                    async: false,
+                    success: function (result) {
+                        if (result.status == 0) {
+                            window.location = window.location
+                        }
+                    }
+                })
+            })
+        }
+    })
     getDataList()
 })
