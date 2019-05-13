8edc824c42171a62bae81c0289a5c4fc08ad99ca
liuenci <154910381@qq.com>
2019-05-13 12:14:10 +0800

    update order manage

--- vbook-fe/static/js/admin/orderTable.js
+++ vbook-fe/static/js/admin/orderTable.js
@@ -42,15 +42,64 @@ function initOrderTable(orderNo) {
                 title: '创建时间',
                 align: 'center',
                 valign: 'middle'
+            },
+            {
+                field: 'opt',
+                title: '操作',
+                width: 320,
+                align: 'center',
+                valign: 'middle',
+                formatter: actionFormatter,
             }
         ]
     });
 }
 
+//操作栏的格式化
+function actionFormatter(value, row, index) {
+    var result = " <button class='btn btn-default btn-info' data-toggle='modal' onclick='get_data(this)'>订单详情</button>";
+    return result;
+}
+
+function get_data(t) {
+    var value = $(t).parents('tr').find('td').eq(1).text()
+    $('#order_item_table').bootstrapTable('destroy');
+    $('#order_item_table').bootstrapTable({
+        url: 'http://localhost:8080/user/order/' + value,
+        method: 'get',
+        toolbar: "#toolbar",
+        pagination: "false",
+        striped: true, // 是否显示行间隔色
+        //search : "true",
+        uniqueId: "id",
+        dataType: "json",
+        pagination: false, // 是否分页
+        sortable: false, // 是否启用排序
+        columns: [
+            {
+                field: 'bookId',
+                title: '编号'
+            },
+            {
+                field: 'bookName',
+                title: '书名'
+            },
+            {
+                field: 'price',
+                title: '价格'
+            },
+            {
+                field: 'quantity',
+                title: '数量'
+            }]
+    });
+    $("#order_items_id").modal("toggle");
+}
+
 function reflashOrderList() {
     // 先销毁表格，再初始化表格
     orderTable.bootstrapTable('destroy');
-    let orderNo = $('#orderNo').val()
+    var orderNo = $('#orderNo').val()
     initOrderTable(orderNo);
 }
 
